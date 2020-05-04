aec3b1cf6b7807bc5aee24d56d878e5400808d12
moamen-ragab <59703721+moamen-ragab@users.noreply.github.com>
2020-05-04 03:51:24 +0200

    nav
    nav

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(document).ready(function ($) {
   $(window).on("load", function () {
     $("#preloader").delay(600).fadeOut();
   });
+  // Sticky Nav
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $("#head").addClass("lt-bg");
+      $("#head a").addClass("dk-co");
+    } else {
+      $("#head").removeClass("lt-bg");
+      $("#head a").removeClass("dk-co");
+    }
+  });
 
   // Go Top Button
   $(window).scroll(function () {
