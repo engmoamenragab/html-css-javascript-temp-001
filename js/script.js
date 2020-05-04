9ad062e2be5f3072ef60fe2c2e39aa6b8bca792c
moamen-ragab <59703721+moamen-ragab@users.noreply.github.com>
2020-05-04 02:54:34 +0200

    go top
    go top

--- js/script.js
+++ js/script.js
@@ -6,35 +6,15 @@ $(document).ready(function ($) {
     $("#preloader").delay(600).fadeOut();
   });
 
-  // Scroll
-  $("body").scrollspy({
-    target: "#nav",
-    offset: $(window).height() / 2,
-  });
-
-  $("#nav .main-nav a[href^='#']").on("click", function (e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $("html, body").animate({ scrollTop: $(this.hash).offset().top }, 600);
-  });
-
   // Go Top Button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#go-top").fadeIn();
+    } else {
+      $("#go-top").fadeOut();
+    }
+  });
   $("#go-top").on("click", function () {
     $("body,html").animate({ scrollTop: 0 }, 600);
-  });
-
-  // Nav Collapse
-  $("#nav .nav-collapse").on("click", function () {
-    $("#nav").toggleClass("open");
-  });
-
-  $(".has-dropdown a").on("click", function () {
-    $(this).parent().toggleClass("open-drop");
-  });
-
-  $(window).on("scroll", function () {
-    var wScroll = $(this).scrollTop();
-    wScroll > 1 ? $("#nav").addClass("fixed-nav") : $("#nav").removeClass("fixed-nav");
-    wScroll > 700 ? $("#go-top").fadeIn() : $("#go-top").fadeOut();
   });
 });
